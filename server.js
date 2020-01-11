d579c0c8df7a5f99d1af81caa21a171ec577204b
Prateek Jain <prateekjain14feb@gmail.com>
2020-01-11 11:36:00 +0530

    added view engine

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+// we’ll be using EJS (Embedded JavaScript). EJS is a templating language.
+app.set('view engine', 'ejs')
+
 app.get('/',function(req, res){
-    res.send("Hello World !!!");
-})
+    // res.send("Hello World !!!");
+    res.render('index');
+});
 
 app.listen(3000,function(){
     console.log('Example app listening on port 3000!');
-})
+});
